8efd86fc1ea6b41f5ad2cf9150c4fc38d1531e58
Raisan <ytpc26ms@outlook.jp>
2021-07-14 19:31:46 +0900

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name = "PyIsset",
+    version = "1.0.0",
+    url = "https://github.com/code-raisan/PyIsset",
+    author = "code-raisan",
+    license = "The MIT License",
+    packages = ["pyis"],
+    description = "Is there a key when referencing dictionary data? Or do you have letters? Includes functions such as.",
+    download_url = "https://code-raisan.github.io/PyIsset/pkg/pyisset-1.0.0.tar.gz"
+)
